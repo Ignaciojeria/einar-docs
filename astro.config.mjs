4875dc6b9135bca2688e421e261ddc50934d9f4d
ignaciojeria <ignaciovl.j@gmail.com>
2024-05-05 19:37:06 -0400

    github action added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ignaciojeria.github.io',
+	base: 'einar-docs',
 	integrations: [
 		starlight({
 			title: 'EinarCLI',
