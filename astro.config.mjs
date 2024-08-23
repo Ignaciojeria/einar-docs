8c38c716c1ba46bac985a4959f1318267ae5a4ea
ignaciojeria <ignaciovl.j@gmail.com>
2024-08-23 01:52:06 -0400

    testing daisyUI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,123 +1,135 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from "@astrojs/tailwind";
 
 export const locales = {
-	root: { label: 'English', lang: 'en' },
-	es: { label: 'Español', lang: 'es' }
+  root: {
+    label: 'English',
+    lang: 'en'
+  },
+  es: {
+    label: 'Español',
+    lang: 'es'
+  }
 };
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ignaciojeria.github.io',
-	integrations: [
-		starlight({
-			title: 'EinarCLI',
-			logo: { 
-                src: './src/assets/logo.svg', // Ruta a tu archivo de logo
-            },
-			social: {
-				github: 'https://github.com/ignaciojeria/einar',
-			},
-			components: {
-				TableOfContents: './src/components/TableOfContents.astro',
-			},
-			locales,
-			sidebar: [
-				{
-					label: 'Start Here',
-					translations: {
-						es: 'Comienza Aquí',
-					},
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{
-						label: 'Getting Started', 
-						link: '/getting-started/' ,
-						translations: {
-							es: 'Empezando',
-						}
-						},	
-					],
-				},
-				{
-					label: 'Core',
-					autogenerate: { directory: 'core' },
-				},
-				{
-					label: 'Logging',
-					autogenerate: { directory: 'logging' },
-				},
-				/*
-				{
-					label: 'JSON Schema',
-					autogenerate: { directory: 'json-schema' },
-				},*/
-				{
-					label: 'OpenAPI',
-					autogenerate: { directory: 'openapi' },
-				},
-				{
-					label: 'OpenTelemetry',
-					autogenerate: { directory: 'open-telemetry' },
-				},
-				{
-					label: 'Tunnel',
-					autogenerate: { directory: 'ngrok' },
-				},
-				/*
-				{
-					label: 'NATS - Synadia Cloud',
-					autogenerate: { directory: 'synadia-cloud' },
-				},*/
-				/*
-				{
-					label: 'Einar AI Gateway',
-					autogenerate: { directory: 'einar-ai-gateway'},
-				},*/
-				{
-					label: 'Google Cloud',
-					autogenerate: { directory: 'google-cloud' },
-				},
-				{
-					label: 'StorJ',
-					autogenerate: { directory: 'storj' },
-				},
-				{
-					label: 'Others',
-					autogenerate: { directory: 'others' },
-				},
-				/*
-				{
-					label: 'Powered By Einar',
-					autogenerate: { directory: 'powered-by-einar'},
-				}*/
-				/*
+  site: 'https://ignaciojeria.github.io',
+  integrations: [starlight({
+    title: 'EinarCLI',
+    logo: {
+      src: './src/assets/logo.svg' // Ruta a tu archivo de logo
+    },
+    social: {
+      github: 'https://github.com/ignaciojeria/einar'
+    },
+    components: {
+      TableOfContents: './src/components/TableOfContents.astro'
+    },
+    locales,
+    sidebar: [{
+      label: 'Start Here',
+      translations: {
+        es: 'Comienza Aquí'
+      },
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Getting Started',
+        link: '/getting-started/',
+        translations: {
+          es: 'Empezando'
+        }
+      }]
+    }, {
+      label: 'Core',
+      autogenerate: {
+        directory: 'core'
+      }
+    }, {
+      label: 'Logging',
+      autogenerate: {
+        directory: 'logging'
+      }
+    },
+    /*
+    {
+    	label: 'JSON Schema',
+    	autogenerate: { directory: 'json-schema' },
+    },*/
+    {
+      label: 'OpenAPI',
+      autogenerate: {
+        directory: 'openapi'
+      }
+    }, {
+      label: 'OpenTelemetry',
+      autogenerate: {
+        directory: 'open-telemetry'
+      }
+    }, {
+      label: 'Tunnel',
+      autogenerate: {
+        directory: 'ngrok'
+      }
+    },
+    /*
+    {
+    	label: 'NATS - Synadia Cloud',
+    	autogenerate: { directory: 'synadia-cloud' },
+    },*/
+    /*
+    {
+    	label: 'Einar AI Gateway',
+    	autogenerate: { directory: 'einar-ai-gateway'},
+    },*/
+    {
+      label: 'Google Cloud',
+      autogenerate: {
+        directory: 'google-cloud'
+      }
+    }, {
+      label: 'StorJ',
+      autogenerate: {
+        directory: 'storj'
+      }
+    }, {
+      label: 'Others',
+      autogenerate: {
+        directory: 'others'
+      }
+    }
+    /*
+    {
+    	label: 'Powered By Einar',
+    	autogenerate: { directory: 'powered-by-einar'},
+    }*/
+    /*
         		{
-					label: 'EinarCLI Powered Projects',
-					autogenerate: { directory: 'synadia-cloud' },
-				},
-	
-				{
-					label: 'Courses (soon)',
-					translations: {
-						es: 'Cursos (pronto)',
-					}
-				}*/
-				/*
-				{
-					label: 'Fermyon Cloud',
-					autogenerate: { directory: 'fermyon-cloud' },
-				},
-				{
-					label: 'Tutorials',
-					autogenerate: { directory: 'tutorials' },
-				},
-				{
-					label: 'Resources',
-					autogenerate: { directory: 'resources' },
-				},
-				*/
-			],
-		}),
-	],
+    	label: 'EinarCLI Powered Projects',
+    	autogenerate: { directory: 'synadia-cloud' },
+    },
+    			{
+    	label: 'Courses (soon)',
+    	translations: {
+    		es: 'Cursos (pronto)',
+    	}
+    }*/
+    /*
+    {
+    	label: 'Fermyon Cloud',
+    	autogenerate: { directory: 'fermyon-cloud' },
+    },
+    {
+    	label: 'Tutorials',
+    	autogenerate: { directory: 'tutorials' },
+    },
+    {
+    	label: 'Resources',
+    	autogenerate: { directory: 'resources' },
+    },
+    */]
+  }), tailwind()]
 });
---
+++ src/content/docs/powered-by-einar/multicomerce.md
@@ -0,0 +1,5 @@
+---
+title: MultiComerce
+description: A reference page in my new Starlight docs site.
+---
+
