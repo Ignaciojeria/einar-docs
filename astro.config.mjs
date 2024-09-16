465b7cb55e6a6c536ccd5eef8ff65bf0f48abb1f
ignaciojeria <ignaciovl.j@gmail.com>
2024-09-16 12:20:24 -0300

    docs improved

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,7 +48,9 @@ export default defineConfig({
       autogenerate: {
         directory: 'core'
       }
-    }, {
+    }, 
+    /*
+    {
       label: 'Logging',
       autogenerate: {
         directory: 'logging'
